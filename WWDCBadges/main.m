3a84522b07c669f615946b2f6c81330267a19218
Yoseob Lee <yoyoyoseob@gmail.com>
2015-05-20 15:26:32 -0400

    Add for loop for nametags

--- WWDCBadges/main.m
+++ WWDCBadges/main.m
@@ -10,11 +10,17 @@
 
 int main(int argc, const char * argv[])
 {
-
+    
     @autoreleasepool {
         
         // insert code here...
         
+        NSArray *conferenceSpeakers = @[@"Edsger", @"Ada", @"Charles", @"Alan", @"Grace", @"Linus", @"Wozniak"];
+        
+        for (NSInteger i = 0; i < 7; i++) {
+            NSLog(@"Hello, my name is %@", conferenceSpeakers[i]);
+            
+        }
     }
     return 0;
 }
